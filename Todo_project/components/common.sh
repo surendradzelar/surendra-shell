849c35c5576d84b5d0d525fed4d13b6c421493f7
zssurendradzelar <surendradudi432@gmail.com>
2021-07-30 09:35:00 +0530

    first

---
+++ Todo_project/components/common.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+Head () {
+     echo -e "\e[1;34m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% $1 %%%%%%%%%%%%%%%%%%%%%%%% \e[0m "
+    echo -e "\e[1;33m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% $1 %%%%%%%%%%%%%%%%%%%%%% \e[0m "  >>$LOG
+}
+
+Stat(){
+    if [ "$1" -eq 0];then
+    echo -e "\e[1;32mSUCCESS\e[0m"
+    else
+    echo -e "\e[1;32mFAIL\e[0m"
+    echo -e "\n\e[1;31m logs in the file,log file path=${LOG}\e[0m"
+    exit 1
+    fi
+}
+
+OS_PREREQ(){
+    set-hostname ${COMPONENT}
+    Head"updating the pacakges"
+    apt update &>>$LOG
+    Stat $?
+}
+   
+ERROR(){
+    echo -e "\e[1;35m$1\e[0m"
+}
+    
+DOWNLOAD_COMPONENT(){
+    Head "updating ${COMPONENT} "
+    git clone "https://github.com/surendradzelar/${COMPONENT}"
+}
---
+++ Todo_project/todos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COMPONENT=$1
+
+export LOG=/tmp/${COMPONENT}.log
+rm -r ${LOG}
+
+source components/common.sh
+if [ ! -f components/${COMPONENT}.sh ];then
+    ERROR "Invalied component provied"
+    exit 1
+fi
+
+USER_NAME=$(whoami)
+
+if [ "${USER_NAME}" != "root"];then
+    ERROR "must be root user to perform the script"
+    exit 1
+fi
+
+export components
+bash components/${COMPONENT}.sh
